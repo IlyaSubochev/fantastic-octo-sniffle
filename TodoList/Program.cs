c2a8da7fa1cb2dcc31612fe191186881b3f8f507
Ilya Subochev <siveternal@gmail.com>
2022-10-25 23:28:36 +0530

    init new project

---
+++ TodoList/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Занимаюсь программированием ПЛК");
+Console.WriteLine("Для дальнейшего развития начал изучать C#");
+Console.WriteLine("Так как увидел на одном проекте делали высокий уровень на шарпах");
+Console.WriteLine("Это очень сильно меня заинтересовало");
+Console.WriteLine("Да и нравится изучать что-то новое, а не сидеть ровно на стуле");
